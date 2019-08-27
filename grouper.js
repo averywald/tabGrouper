ef545bff7a39163bf5cb8e3f887490a8a6a493b1
averywald <averywald.dev@gmail.com>
2019-08-27 01:27:13 -0500

    new regex url filter

--- grouper.js
+++ grouper.js
@@ -2,7 +2,7 @@
 // avery wald
 
 // simple error logger
-function onError(error) { console.log(`Error: ${error}`); }
+function onError(e) { console.log(e); }
 
 // sorts tabs by url and reorders them in the browser
     // grouping similar domains, thus organizing them for easy tab cycling
@@ -15,7 +15,13 @@ function grouper(windowInfo) {
     }
 
     // sort tab objects
-    var newTabs = [...windowInfo.tabs].sort((a, b) => (a.url > b.url) ? 1 : -1);
+    let newTabs = [...windowInfo.tabs].sort((a, b) => {
+        // strip http protocol from url strings
+        let c = a.url.replace(/^(?:https?:\/\/)?(?:www\.)?/i, '');
+        let d = b.url.replace(/^(?:https?:\/\/)?(?:www\.)?/i, '');
+        // compare and return sort protocol
+        return (c > d) ? 1 : -1;
+    });
 
     // recursively move all tabs to front in sorted order
     browser.tabs.move(newTabs.map(t => t.id), { index: -1 });
@@ -27,14 +33,17 @@ function grouper(windowInfo) {
 function driver() {
 
     // get current tab
-    var current = browser.windows.getCurrent({ populate: true });
+    let current = browser.windows.getCurrent({ populate: true });
     // call grouper function
     current.then(grouper, onError);
 
 }
 
 // execute grouping on button click
-browser.browserAction.onClicked.addListener(() => driver);
+browser.browserAction.onClicked.addListener(driver);
 
 // execute grouping on key combo
 browser.commands.onCommand.addListener((cmd) => (cmd == 'toggle-feature') ? driver() : null);
+
+// execute grouping when a new tab is opened
+browser.tabs.onCreated.addListener(driver);
