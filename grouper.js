b5bc1ed1b9810f70c8e691321f2d72f3620b4a55
averywald <averywald.dev@gmail.com>
2019-08-27 00:25:40 -0500

    added key command feature

--- grouper.js
+++ grouper.js
@@ -2,38 +2,39 @@
 // avery wald
 
 // simple error logger
-function onError(error) {
-    console.log(`Error: ${error}`);
-}
+function onError(error) { console.log(`Error: ${error}`); }
 
 // sorts tabs by url and reorders them in the browser
     // grouping similar domains, thus organizing them for easy tab cycling
-
 function grouper(windowInfo) {
 
     // if no open tabs
     if (windowInfo.tabs.length == 0) {
-        return;
+        // handle error
+        onError('No active tabs...nothing for me to do');
     }
 
     // sort tab objects
     var newTabs = [...windowInfo.tabs].sort((a, b) => (a.url > b.url) ? 1 : -1);
 
-    // move all tabs to front in sorted order
-    let moving = browser.tabs.move(newTabs.map(t => t.id), { index: -1 });
-    moving.then((newTabs) => {
-        console.log('grouped tabs');
-    }, onError);
+    // recursively move all tabs to front in sorted order
+    browser.tabs.move(newTabs.map(t => t.id), { index: -1 });
 
 }
 
-// event-driven 'on click' function
-    // handles states and calls functions
-browser.browserAction.onClicked.addListener(() => {
+// browser tabs API hook
+    // calls grouper function
+function driver() {
 
     // get current tab
     var current = browser.windows.getCurrent({ populate: true });
     // call grouper function
     current.then(grouper, onError);
 
-});
+}
+
+// execute grouping on button click
+browser.browserAction.onClicked.addListener(() => driver);
+
+// execute grouping on key combo
+browser.commands.onCommand.addListener((cmd) => (cmd == 'toggle-feature') ? driver() : null);
